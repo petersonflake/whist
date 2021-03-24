0ba0cd4e33276e2d1af286668396646576212f35
Mitchell Flaherty <mbfmitchell@gmail.com>
2021-03-24 16:46:17 -0500

    AI Improvements
    AI now accounts for trumps when deciding what card to play.

--- ai.c
+++ ai.c
@@ -78,13 +78,30 @@ card* choose_card(stack *hand, trick *t, suits trumps)
                 return get_card(weakest_card, hand);
         }
         /* Only play strongest card if it would not take trick from an ally */
+
+        /* By the time we reach here, we know our strongest is better than any
+         * other card currently on the trick because of the loop above, so we
+         * are just making sure we do not take the trick from our teammate
+         * unnecessarily.
+         */
         switch(t->count) {
+            /* Playing second card of trick, attempt to take it from opponents */
             case 1:
-            case 3:
                 return get_card(strongest_card, hand);
                 break;
+            /* Playing third card of trick.  If ally played weak card, attempt to take trick
+             * Otherwise, play weak card to avoid stealing from ally */
             case 2:
-                return get_card(weakest_card, hand);
+                if(t->cards[0]->rank < 7 || t->cards[1]->rank > t->cards[0]->rank)
+                    return get_card(strongest_card, hand);
+                else
+                    return get_card(weakest_card, hand);
+            case 3:
+            /* Playing fourth card of trick.  Play strongest if ally is weak */
+                if(t->cards[1]->rank < t->cards[0]->rank || t->cards[1]->rank < t->cards[2]->rank)
+                    return get_card(strongest_card, hand);
+                else
+                    return get_card(weakest_card, hand);
                 break;
             default:
                 fprintf(stderr, "Trick has wrong number of cards: %d\n", t->count);
@@ -104,8 +121,11 @@ card* choose_card(stack *hand, trick *t, suits trumps)
             }
         }
         card *weakest_card = NULL;
+        card *weakest_trump = NULL;
+        card *strongest_trump = NULL;
         int i;
         for(i = 0; i < hand->count; ++i) {
+            /* Get weakest card of weakest suit */
             if(weakest_card) {
                 if(hand->cards[i]->suit == weakest_suit)
                     if(hand->cards[i]->rank < weakest_card->rank)
@@ -113,6 +133,24 @@ card* choose_card(stack *hand, trick *t, suits trumps)
             } else {
                 if(hand->cards[i]->suit == weakest_suit)
                     weakest_card = hand->cards[i];
+            }
+            /* Get weakest card of trump suit, if any */
+            if(weakest_trump) {
+                if(hand->cards[i]->suit == trumps)
+                    if(hand->cards[i]->rank < weakest_trump->rank)
+                        weakest_trump = hand->cards[i];
+            } else {
+                if(hand->cards[i]->suit == trumps)
+                    weakest_trump = hand->cards[i];
+            }
+            /* Get strongest card of trump suit, if any */
+            if(strongest_trump) {
+                if(hand->cards[i]->suit == trumps)
+                    if(hand->cards[i]->rank > strongest_trump->rank)
+                        strongest_trump = hand->cards[i];
+            } else {
+                if(hand->cards[i]->suit == trumps)
+                    strongest_trump = hand->cards[i];
             }
         }
         if(!weakest_card) {
@@ -122,6 +160,49 @@ card* choose_card(stack *hand, trick *t, suits trumps)
             printf("Unable to choose a card.\n");
             abort();
         }
-        return get_card(weakest_card, hand);
+        /* If we do not have any trump cards, just return weakest card */
+        if(!weakest_trump || !strongest_trump) {
+            return get_card(weakest_card, hand);
+        }
+        /* TODO: Check logic.  I suspect it is sub-optimal */
+        switch(t->count) {
+            /* We are playing second card.  Trump opponent if possible */
+            case 1:
+                return get_card(strongest_trump, hand);
+                break;
+            /* We are playing third card.  Trump opponent if ally has not done so. */
+            case 2:
+                if(t->cards[1]->suit == trumps && strongest_trump->rank > t->cards[1]->rank)
+                    return get_card(strongest_trump, hand);
+                else if((t->cards[1]->suit == lead) && t->cards[1]->rank > t->cards[0]->rank)
+                    return get_card(strongest_trump, hand);
+                else if(t->cards[1]->suit == lead && t->cards[1]->rank < t->cards[0]->rank)
+                    return get_card(weakest_card, hand);
+                else
+                    return get_card(weakest_trump, hand);
+                break;
+
+            /* We are playing the fourth card.  Trump opponent if that would be effective. */
+            case 3:
+                if(t->cards[2]->suit == trumps && t->cards[1]->suit != trumps && strongest_trump->rank > t->cards[2]->rank) {
+                    return get_card(strongest_trump, hand);
+
+                } else if(t->cards[2]->suit == trumps && t->cards[1]->suit == trumps && t->cards[1]->rank < t->cards[2]->rank) {
+                    if(strongest_trump->rank > t->cards[2]->rank) {
+                        return get_card(strongest_trump, hand);
+                    } else {
+                        return get_card(weakest_card, hand);
+                    }
+                }
+                else if(t->cards[2]->rank != trumps && t->cards[1]->suit == trumps)
+                    return get_card(weakest_card, hand);
+                else
+                    return get_card(weakest_trump, hand);
+
+            default:
+                fprintf(stderr, "Trick has wrong number of cards.\n");
+                return NULL;
+
+        }
     }
 }
